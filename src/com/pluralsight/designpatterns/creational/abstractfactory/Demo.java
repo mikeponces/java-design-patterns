17a72e8449e820bc4900a694d84ce7fb2effc7a9
msanchez <msanchez@localhost>
2023-03-21 11:58:45 +0800

    Fix warning.

--- src/com/pluralsight/designpatterns/creational/abstractfactory/Demo.java
+++ src/com/pluralsight/designpatterns/creational/abstractfactory/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	public static void main(String[] args) {
 		CreditCardFactory abstractFactory = CreditCardFactory.get(649);
 		CreditCard card = abstractFactory.getCreditCard(CardType.PLATINUM);
+		
+		System.out.println(card);
 
 //		CreditCardFactory abstractFactory = CreditCardFactory.get(651);
 //		CreditCard card = abstractFactory.getCreditCard(CardType.GOLD);
